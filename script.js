3f2921c2358474a396ce55cca7e412876d8e91e2
Najiba-Haidari <nhaidari089@gmail.com>
2023-06-15 17:53:03 -0600

    updated

--- script.js
+++ script.js
@@ -1,67 +1,89 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
+var upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'];
+// var upperChar = lowChar.map(function (x) { return x.toUpperCase(); })
+// console.log(upperChar);
+var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var lowChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperChar= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z","Y","X"];
-var specialChar = ["~","!","@","#","$","%","^","&","*","(",")","{","}","[","]","_",".",",","+","-","=","?","<",">"];
+function generatePassword() {
 
-// var numbers = "0123456789";
-// var lowChar ="abcdefghijklmnopqrstuvwxyz";
-// var upperChar= "ABCDEFGHIJKLMNOPQRSTUVWZYX";
-// var specialChar = "~!@#$%^&*(){}[]_.,+-=?<>";
+  var finalPassword = "";
 
-
-function generatePassword(){
-  var finalPassword="";
-
-  var passwordLength = prompt("How many characters would you like your password to contain between 8 to 128?");
+  var passwordLength = prompt("Please enter length of your password between 8-128");
 
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("Select the range between 8-128");
-    var passwordLength = prompt("How many characters would you like your password to contain between 8 to 128?");
+    alert("Please select the range between 8-128");
+    var passwordLength = prompt("Please mention length of your password between 8-128");
   }
 
-  var confirmLowerCase = confirm("Do you wish to add lower case letters?");
-  var confirmUpperCase = confirm("Do you wish to add the Upper case letters?");
-  var confirmkNum = confirm("Do you wish to add numbers?");
-  var confirmSpecialChar = confirm("Do you wish to add special characters here?");
+  // if(!passwordLength){
 
+  // }
 
-  for (let i = 0; i < passwordLength; i++) {
-    if (confirmLowerCase === true && finalPassword.length < passwordLength){
-      var position = Math.floor(Math.random() * lowChar.length);
-      finalPassword = finalPassword + lowChar[position];
-    }
-    if (confirmUpperCase === true && finalPassword.length < passwordLength){
-      var position = Math.floor(Math.random() * upperChar.length);
-      finalPassword = finalPassword + upperChar[position];
-    }
-    if (confirmkNum === true && finalPassword.length < passwordLength){
-      var position = Math.floor(Math.random() * numbers.length);
-      finalPassword = finalPassword + numbers[position];
-    }
-    if (confirmSpecialChar === true && finalPassword.length < passwordLength){
-      var position = Math.floor(Math.random() * specialChar.length);
-      finalPassword = finalPassword + specialChar[position];
-    }
+  var confirmNumber = confirm("Do you like to add numbers in your password?");
+  var confirmLowChar = confirm("Do you like to add lower case characters in your password?");
+  var confirmUpperChar = confirm("Do you like to add Upper case characters in your password?");
+  var confirmSpecialChar = confirm("Do you like to add special characters in your password?");
 
+for (let i = 0; i < passwordLength; i++) {
+  if(confirmNumber === true && finalPassword.length < passwordLength){
+    var index = Math.floor(Math.random() * numbers.length);
+    finalPassword = finalPassword + numbers[index]
   }
-    return finalPassword;
+
+  if(confirmLowChar === true && finalPassword.length < passwordLength){
+    var index = Math.floor(Math.random() * lowChar.length);
+    finalPassword = finalPassword + lowChar[index];
+  }
+
+  if(confirmUpperChar === true && finalPassword.length < passwordLength){
+    var index = Math.floor(Math.random() * upperChar.length);
+    finalPassword = finalPassword + upperChar[index];
+  }
+
+  if(confirmSpecialChar === true && finalPassword.length < passwordLength){
+    var index = Math.floor(Math.random() * specialChar.length);
+    finalPassword = finalPassword + specialChar[index];
+  }
 }
 
+return finalPassword;
+}
+
+// generatePassword();
+
 // Write password to the #password input
-
-generatePassword ();
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
+function deletePassword(){
+  var passwordText = document.querySelector("#password");
+  passwordText.value = '';
+}
+
+function copyPassword(){
+  var passwordText = document.getElementById("password");
+  passwordText.select();
+  passwordText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(passwordText.value);
+ 
+  // alert("copied!")
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var deleteBtn = document.querySelector("#delete");
+deleteBtn.addEventListener("click", deletePassword);
+
+var copyBtn = document.querySelector("#copy");
+copyBtn.addEventListener("click", copyPassword);
+
